52d06937a19cb5c5b960de779c90555d546798fd
Manuel Limones <limonesu.me@gmail.com>
2017-10-11 17:42:04 -0500

    bundle css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import $ from 'jquery'
+//import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import './custom-styles.css'
 
 $(function(){
+  $('#myButton').click(function() {
+    console.log('Click on: left');
+  });
+
+  $('#otherButton').click(function() {
+    console.log('Click on: right');
+  });
   console.log('Hello, World!');
 });
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: "./src/main.js",
@@ -9,5 +10,18 @@ module.exports = {
   },
   devServer: {
     contentBase: 'public'
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({fallback: "style-loader", use: "css-loader"})
+      },
+      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
+    ]
   }
 };
